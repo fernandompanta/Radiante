6f18412e2192a345d0a096bd727e34bb75f4544f
fernandompanta <fernandompanta@yahoo.com.br>
2026-08-24 17:23:42 -0300

    Emitir_relatorio_ATV_WF_insucesso_1.0
    //Robô para emissão de relatório do WF da Claro utilizando senha de terceiros com foco no motivo dos Insucessos.

---
+++ Emitir_relatorio_ATV_WF_insucesso_1.0.js
@@ -0,0 +1,151 @@
+//Emitir_relatorio_ATV_WF_insucesso_1.0.js
+
+//Robô para emissão de relatório do WF da Claro utilizando senha de terceiros com foco no motivo dos Insucessos.
+
+
+const { chromium } = require('playwright');
+
+
+async function main() {
+
+    const browser = await chromium.launch({
+        headless: false,
+        args: ['--force-device-scale-factor=1.0']
+    });
+
+    const page = await browser.newPage();
+
+
+    // ============================================================
+    // LOGIN
+    // ============================================================
+    await page.goto('https://secure.embratel.com.br/wfep/NovoLogin.aspx');
+    await page.getByRole('textbox', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Login' }).fill('z495506');
+    await page.getByRole('textbox', { name: 'Senha' }).click();
+    await page.getByRole('textbox', { name: 'Senha' }).fill('Y9a.(Pd@#');
+    console.log(' ');
+    console.log('Inicializando WF para emição de relatório...');
+
+
+    // ============================================================
+    // AGUARDA PARA DIGITAR O CAPTCHA
+    // ============================================================
+    console.log(' ');
+    console.log('Validando capctha do site...');
+    await page.waitForNavigation();
+    await page.waitForTimeout(1000);
+
+
+    // ============================================================
+    // ENTRANDO NA TELA DE CONSULTA
+    // ============================================================
+    await page.getByRole('link', { name: 'Consulta', exact: true }).click();
+    console.log(' ');
+    console.log('Entrando na tela de Consulta');
+    await page.waitForTimeout(500);
+    await page.getByLabel('Categoria do Item:').selectOption('3');
+    await page.waitForTimeout(2000);
+    //await page.goto('https://secure.embratel.com.br/wfep/relatorios/consulta/default.aspx');
+    await page.waitForTimeout(1000);
+    await page.locator('td:nth-child(4) > a:nth-child(2)').click();
+    await page.waitForTimeout(1000);
+
+
+    // ============================================================
+    // SELECIONANDO ITENS QUE SERÃO EMITIDOS NO RELATÓRIO
+    // ============================================================
+    await page.getByRole('checkbox', { name: 'Atividade' }).check();
+    console.log(' ');
+    console.log('Selecionando Atividade');
+    await page.waitForTimeout(500);
+
+    await page.getByRole('checkbox', { name: 'Data Aprovação da Medição' }).check();
+    console.log('Selecionando Data Aprovação da Medição');
+    await page.waitForTimeout(500);
+
+    await page.getByRole('checkbox', { name: 'Responsável', exact: true }).uncheck();
+    console.log('Desmarcando Responsável');
+    await page.waitForTimeout(500);
+
+    await page.getByRole('checkbox', { name: 'Total da Medição' }).check();
+    console.log('Selecionando Total da Medição');
+    await page.waitForTimeout(500);
+
+    await page.getByRole('checkbox', { name: 'Motivo de Insucesso' }).check();
+    console.log('Selecionando Motivo de Insucesso');
+    await page.waitForTimeout(500);
+
+    await page.getByRole('button', { name: 'Fechar' }).click();
+    console.log(' ');
+    console.log('Seleções executadas com sucesso!');
+    await page.waitForTimeout(500);
+
+    await page.locator('#MainContent_ddlCampoPesquisa_0').selectOption('437_Data_Data Aprovação da Medição');
+    console.log(' ');
+    console.log('Alocando filtro para Data Aprovação da Medição');
+    await page.waitForTimeout(2000);
+
+    await page.locator('#MainContent_ddlCriterioPesquisa_0').selectOption('BETWEEN');
+
+
+    // ============================================================
+    // SELECIONANDO A DATA INCIAL E FINAL DO MÊS ANTERIOR
+    // ============================================================
+    // 1. Selcionando o primeiro dia do mês anterior
+    await page.getByRole('textbox', { name: 'De:' }).click();
+    await page.waitForTimeout(500);
+    await page.getByTitle('Anterior').click();
+    await page.waitForTimeout(500);
+    await page.getByRole('link', { name: '1', exact: true }).click();
+    await page.waitForTimeout(500);
+ 
+
+    // Calculando o último dia do mês anterior
+        const hoje = new Date();
+            const ultimoDiaObj = new Date(hoje.getFullYear(), hoje.getMonth(), 0);
+        const ultimoDia = ultimoDiaObj.getDate().toString(); // Retorna "31", "30", "28" ou "29"
+
+    // Selecionando o ultimo dia do mês anterio de forma dinâmica
+    await page.getByRole('textbox', { name: 'Até:' }).click();
+    await page.waitForTimeout(500);
+    await page.getByTitle('Anterior').click();
+    await page.waitForTimeout(500);
+    await page.getByRole('link', { name: ultimoDia, exact: true }).click();
+    await page.waitForTimeout(500);
+
+
+    // Clicando em Pesquisar para iniciar a emissão do relatório
+    await page.getByRole('link', { name: ' Pesquisar' }).click();
+    console.log(' ');
+    console.log('Emitindo relatório, por favor aguarde, isso pode demorar...');
+    await page.waitForTimeout(30000);
+
+
+    // Aguarda o elemento ficar visível no DOM e na tela
+    //const botaoExportar = page.getByRole('button', { name: 'Exportar resultado para Excel' });
+    
+    //await botaoExportar.waitFor({ state: 'visible' });
+
+    
+    // Aviso de Relatório emitido com sucesso!!
+
+    //await page.waitForTimeout(1000);
+    //console.log(' ');
+    //console.log('Relatório emitido com sucesso!!');
+
+    
+    //await page.waitForNavigation();
+    //await page.getByRole('button', { name: 'E n t r a r' }).click();
+
+
+    // Mantém o programa aberto até você pressionar Ctrl + C no terminal
+    //await new Promise(() => {});
+
+    //Comando para abrir o Codegen
+    //npx playwright codegen https://secure.embratel.com.br/wfep/NovoLogin.aspx
+    
+
+    }
+
+main();
